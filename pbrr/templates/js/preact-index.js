1e1f6b3381d8300b5ff9b2c68671da5c03d77c48
Kartones <diego@kartones.net>
2022-08-14 19:46:52 +0200

    serve local preact js files + remove unused old js

---
+++ pbrr/templates/js/preact-index.js
@@ -0,0 +1,5 @@
+import { h as r } from "/js/preact.module.js";
+export { h, render, Component } from "/js/preact.module.js";
+import e from "/js/htm.module.js";
+const m = e.bind(r);
+export { m as html };
